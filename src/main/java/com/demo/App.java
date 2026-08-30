9b8ce822d2a9b92a1f7a2c6aa330d76be87900c6
MENOB <senjumenob@gmail.com>
2026-08-29 21:23:12 -0300

    Atualização de comentário em App.java para teste de workflow

--- src/main/java/com/demo/App.java
+++ src/main/java/com/demo/App.java
@@ -2,17 +2,14 @@ package com.demo;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.SwingConstants;
+import javax.swing.SwingConstants;//para usar o center
 
-/**
- * Aplicação desktop de exemplo (Swing) usada para demonstrar
- * build de software desktop dentro de um container Docker.
- *
- * Este app NÃO precisa ser executado com interface gráfica real
- * durante o build/CI. O container é usado apenas para compilar
- * e empacotar o .jar — a execução com janela acontece na máquina
- * final do usuário.
+/*
+   Aplicação desktop de exemplo (Swing) usada para demonstrar build de software desktop dentro de um container Docker.
+   Este app NÃO precisa ser executado com interface gráfica real durante o build/CI. O container é usado apenas para compilar
+   e empacotar o .jar — a execução com janela acontece na máquina final do usuário.
  */
+
 public class App {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Demo Desktop - CI/CD com Docker");
